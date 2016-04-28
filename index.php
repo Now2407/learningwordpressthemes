99d604f6ab73fd16b990eed3e7875d2d9df030b2
Noah Marriott <noahmm798@gmail.com>
2016-04-27 18:11:12 -0700

    fixing files

---
+++ index.php
@@ -0,0 +1,19 @@
+
+<?php
+
+if(have_posts()){
+    while(have_posts()){
+        the_post();
+        ?>
+<h2><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
+        <?php
+        
+        the_content();
+    }
+}else{
+    
+    echo "<p> not content found</p>";
+    
+}
+        ?>
+
